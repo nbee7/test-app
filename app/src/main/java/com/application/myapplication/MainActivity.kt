eab19fdba83b4d8df484a118b7652a7d26ec717b
nbee7 <daffadit0721@gmail.com>
2023-03-02 11:43:20 +0700

    feature four

--- app/src/main/java/com/application/myapplication/MainActivity.kt
+++ app/src/main/java/com/application/myapplication/MainActivity.kt
@@ -13,4 +13,8 @@ class MainActivity : AppCompatActivity() {
     private fun featureThree() {
         print("feature three")
     }
+
+    private fun featureFour() {
+        print("four")
+    }
 }
